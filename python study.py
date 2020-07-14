61402205c5e5c409467669b42996d801ea4d8b5c
tech-jin-coder <66165178+tech-jin-coder@users.noreply.github.com>
2020-07-14 16:14:09 +0800

    上传玩儿
    我就想上传 你管我嘞

---
+++ python study.py
@@ -0,0 +1,9 @@
+print("......我爱叶俊......")
+temp = input("不妨猜一下我的心里想的是哪个数字：")
+guess = int(temp)
+if guess == 8:
+    print("卧槽，你怎么知道")
+    print("哼，猜中了也没有奖励")
+else:
+   print("猜错了，我现在想的是8")
+print("好无聊，不玩儿了")
